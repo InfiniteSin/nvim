b8e2de02e93c3e8d0d93a5b1aac651e9124dd0fc
Zhou Nan <30224749+InfiniteSin@users.noreply.github.com>
2026-04-08 23:44:58 +0800

    Create fuff lsp config

---
+++ after/lsp/ruff.lua
@@ -0,0 +1,19 @@
+return {
+	settings = {
+		configurationPreference = 'editorFirst',
+		exclude = {
+		  '/tests/',
+		},
+		lineLength = 100,
+		organizeImports = true,
+		fixAll = true,
+		configuration = {
+			lint = {
+				enable = true,
+			},
+			format = {
+				['quote-style'] = 'single',
+		    },
+		},
+	},
+}
